44eb840b6536c8a852a36ba34c2205e7475ea368
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-19 15:25:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import { images, lastImgIndex } from "./images";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+
+  const handlePrevClick = useCallback(
+    () => setCurrentIndex(currentIndex === 0 ? lastImgIndex : currentIndex - 1),
+    [currentIndex]
+  );
+  const handleNextClick = useCallback(
+    () => setCurrentIndex(currentIndex === lastImgIndex ? 0 : currentIndex + 1),
+    [currentIndex]
+  );
+
+  useEffect(() => {
+    if (paused) return;
+    const intervalId = setInterval(() => handleNextClick(), 1750);
+    return () => clearInterval(intervalId);
+  }, [handleNextClick, paused]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="carousel">
+      <div className="image-container">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`${index + 1}`}
+            style={{
+              transform: `translateX(${-currentIndex * 500}px)`,
+              transition: "transform 0.5s ease-out",
+            }}
+          />
+        ))}
+      </div>
+      <div className="buttons-container">
+        <button className="btn" onClick={handlePrevClick}>
+          Prev
+        </button>
+        <button
+          className={`btn ${paused && "blink_me"}`}
+          onClick={() => setPaused(!paused)}
         >
-          Learn React
-        </a>
-      </header>
+          {paused ? "Resume" : "Pause"}
+        </button>
+        <button className="btn" onClick={handleNextClick}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,9 @@
+export const images = [
+  "https://images.unsplash.com/photo-1599394022918-6c2776530abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1458&q=80",
+  "https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+  "https://images.unsplash.com/photo-1599423300746-b62533397364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+  "https://images.unsplash.com/photo-1599561046251-bfb9465b4c44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80",
+  "https://images.unsplash.com/photo-1452698325353-b90e60289e87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  "https://images.unsplash.com/photo-1456254394237-131c81cd1f58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1750&q=80",
+];
+  export const lastImgIndex = images.length - 1
